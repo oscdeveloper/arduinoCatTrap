537369bee02e22a4a0767f060bed51b9057d8d57
Arkadiusz Krakiewicz <biuro@oscdeveloper.pl>
2019-08-14 01:08:11 +0200

    Add GSM module

--- arduinoCatTrap.ino
+++ arduinoCatTrap.ino
@@ -1,8 +1,12 @@
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
 #define PIN_SERVO 3
 #define PIN_SENSOR 4
 #define PIN_SERVO_TRANSISTOR 5
+
+SoftwareSerial GSM(7, 8); // RX, TX
+char phone_no[]="+XXYYYYYYYYY"; // your phone number to receive sms with country prefix
 
 Servo servo;
 byte sensorDetect;
@@ -17,39 +21,78 @@ void setup() {
   
   pinMode(PIN_SERVO, OUTPUT);
   attachServo();
+  
   servo.write(0);
-  delay(1000);
+  delay(500);
+  
   servo.detach();
   digitalWrite(PIN_SERVO_TRANSISTOR, HIGH); // switch off servo power
+
+  GSM.begin(9600);
+  Serial.begin(9600);
+
+  // synchronize A6 module with Arduino 9600 boudrate
+  for( int i=0; i<=10; i++) {
+    delay(500);
+    GSM.println("AT");  
+  }
+  delay(5000);
+
+  GSM.println("AT+CMGF=1"); // select sms text mode
+  delay(200);
+
+  GSM.println("AT+CMGD=1,4"); // delete all sms from memory
+  delay(200);
+
+  sendSMS("Cat Trap powered on");  
+  delay(5000);
+  
+  // Not really necessary but prevents the serial monitor from dropping any input
+  while(GSM.available()) {
+    Serial.write(GSM.read());
+  }   
 }
 
 void loop() {
 
-  delay(100);
+  delay(50);
   
   sensorDetect = digitalRead(PIN_SENSOR);
 
   if ( sensorStatus == false && sensorDetect == LOW ) { // Sensor ON
+    
     attachServo();
     digitalWrite(PIN_SERVO_TRANSISTOR, LOW); // switch on servo power
     delay(100);
+    
     servo.write(180);
-    delay(1000);
-    servo.detach();
-    digitalWrite(PIN_SERVO_TRANSISTOR, HIGH); // switch off servo power
-    sensorStatus = true;
-  } else if ( sensorStatus == true && sensorDetect == HIGH ) { // Sensor OFF
-    attachServo();
-    digitalWrite(PIN_SERVO_TRANSISTOR, LOW); // switch on servo power
-    delay(100);
+    delay(2000);
+    
     servo.write(0);
     delay(1000);
+    
     servo.detach();
     digitalWrite(PIN_SERVO_TRANSISTOR, HIGH); // switch off servo power
-    sensorStatus = false;
+
+    sendSMS("Cat is trapped!");  
+    delay(5000);
+    
+    sensorStatus = true; // trigger servo only once
   }
 }
 
 void attachServo() {
   servo.attach(PIN_SERVO, 500, 2000);
+}
+
+void sendSMS(String msg) {
+  GSM.print("AT+CMGS=\"");
+  GSM.print(phone_no); 
+  GSM.write(0x22);  // double quotes "
+  GSM.write(0x0D);  // hex equivalent of Carraige return    
+  GSM.write(0x0A);  // hex equivalent of newline
+  delay(1000);
+  GSM.print(msg);
+  delay(500);
+  GSM.println (char(26));//the ASCII code of the ctrl+z is 26  
 }
